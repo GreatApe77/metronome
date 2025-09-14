daa1c2b4aadb8785d31e49c9ca06b4f37e57dc81
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-09-14 00:26:26 -0300

    add animation

--- lib/view/widgets/measure_bar.dart
+++ lib/view/widgets/measure_bar.dart
@@ -1,6 +1,48 @@
 import 'package:flutter/material.dart';
 
-class MeasureBar extends StatelessWidget {
+class MeasureBar extends StatefulWidget {
+  final int? currentIndex;
+  final int notesPerMeasure;
+  static const double _height = 15;
+
+  const MeasureBar({
+    super.key,
+    this.currentIndex,
+    required this.notesPerMeasure,
+  });
+
+  @override
+  State<MeasureBar> createState() => _MeasureBarState();
+}
+
+class _MeasureBarState extends State<MeasureBar> {
+  static const Duration _duration = Duration(milliseconds: 200);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(widget.notesPerMeasure, (index) {
+        final bool isActive = widget.currentIndex == index;
+        return AnimatedContainer(
+          duration: _duration,
+          height: MeasureBar._height,
+          width: MeasureBar._height,
+          decoration: BoxDecoration(
+            color:
+                isActive
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).colorScheme.onSurfaceVariant,
+            borderRadius: BorderRadius.circular(
+              isActive ? 0 : MeasureBar._height / 2,
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+/* class MeasureBar extends StatelessWidget {
   final int? currentIndex;
   final int notesPerMeasure;
   static const double _height = 15;
@@ -37,3 +79,4 @@ class MeasureBar extends StatelessWidget {
     );
   }
 }
+ */
